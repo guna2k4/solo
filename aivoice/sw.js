296b2f94bcadd85f4f0212ed0f570983e22d5feb
Guna <155782383+guna2k4@users.noreply.github.com>
2025-04-08 22:49:13 +0530

    Add files via upload

--- aivoice/sw.js
+++ aivoice/sw.js
@@ -1,25 +1,43 @@
 const CACHE_NAME = 'sl-tracker-v1';
+
+// Basic files to cache
 const urlsToCache = [
-    '/',
-    '/index.html',
-    '/styles.css',
-    '/components/XPSystem.js',
-    '/components/DashboardForm.js',
-    'https://unpkg.com/react@18/umd/react.production.min.js',
-    'https://unpkg.com/react-dom@18/umd/react-dom.production.min.js',
-    'https://unpkg.com/babel@7.22.5/standalone/babel.min.js'
+    './',
+    './index.html',
+    './styles.css',
+    './components/XPSystem.js',
+    './components/DailyQuests.js',
+    './components/VoiceRecorder.js',
+    './components/DashboardForm.js'
 ];
 
-self.addEventListener('install', event => {
+// Install service worker
+self.addEventListener('install', (event) => {
     event.waitUntil(
         caches.open(CACHE_NAME)
-            .then(cache => cache.addAll(urlsToCache))
+            .then((cache) => {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+            .catch((err) => {
+                console.log('Cache error:', err);
+            })
     );
 });
 
-self.addEventListener('fetch', event => {
+// Fetch resources
+self.addEventListener('fetch', (event) => {
     event.respondWith(
         caches.match(event.request)
-            .then(response => response || fetch(event.request))
+            .then((response) => {
+                // Cache hit - return response
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+            .catch((err) => {
+                console.log('Fetch error:', err);
+            })
     );
 });
